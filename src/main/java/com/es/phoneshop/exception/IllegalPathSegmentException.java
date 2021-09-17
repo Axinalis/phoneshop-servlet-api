b3e5f0146238301025eb18cb56d6df133fe8e0c9
Axinalis <antontrus@gmail.com>
2021-09-17 14:18:54 +0300

    Changed name of excption

---
+++ src/main/java/com/es/phoneshop/exception/IllegalPathSegmentException.java
@@ -0,0 +1,12 @@
+package com.es.phoneshop.exception;
+
+public class IllegalPathSegmentException extends RuntimeException {
+
+	public IllegalPathSegmentException(String message) {
+		super(message);
+	}
+
+	public IllegalPathSegmentException() {
+	}
+
+}
