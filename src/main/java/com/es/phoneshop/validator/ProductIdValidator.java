267f589f2703083f745173d75d95b2b0e7a2b41c
Axinalis <antontrus@gmail.com>
2021-09-17 14:18:54 +0300

    Changed name of excption

--- src/main/java/com/es/phoneshop/validator/ProductIdValidator.java
+++ src/main/java/com/es/phoneshop/validator/ProductIdValidator.java
@@ -1,18 +1,18 @@
 package com.es.phoneshop.validator;
 
-import com.es.phoneshop.exception.IdIsNotValidException;
+import com.es.phoneshop.exception.IllegalPathSegmentException;
 
 public class ProductIdValidator {
 
 	public static Long validadingId(String productId) {
 		
 		if(productId == null) {
-			throw new IdIsNotValidException("Id is null");
+			throw new IllegalPathSegmentException("Id is null");
 		}
 		try {
 			return Long.valueOf(productId.substring(1));
 		} catch(NumberFormatException ex) {
-			throw new IdIsNotValidException("Cannot parse id to Long");
+			throw new IllegalPathSegmentException("Cannot parse id to Long");
 		}
 		
 	}
